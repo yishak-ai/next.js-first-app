f446e995cc347116ec49fddeba3601ea40c7e0a2
Gishen21 <teferayishak52@gmail.com>
2025-08-13 10:43:07 -0700

    rewrite active link

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,41 +1,39 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-const navLinks = [
-  { name: "register", href: "/register" },
-  { name: "login", href: "/login" },
-  { name: "forgot password", href: "/forgotpassword" },
-];
-export default function Authlayout({
+
+export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathName = usePathname();
+  const links = [
+    { href: "/login", name: "Login" },
+    { href: "/register", name: "Register" },
+    { href: "/forgotpassword", name: "Forgot Password" },
+  ];
+  const pathname = usePathname();
   return (
     <>
-      <div className="flex  flex-col justify-center items-center ">
-        <nav className="flex my-6 w-fit bg-slate-400 flex-col px-4 py-10 rounded-ss-2xl rounded-ee-xl justify-center items-center gap-2">
-          {navLinks.map((link, index) => {
-            const isActive =
-              pathName === link.href ||
-              (pathName.startsWith(link.href) && link.href !== "/");
+      <div>
+        <ul>
+          {links.map((link, index) => {
+            const isactive =
+              pathname === link.href ||
+              (pathname === link.href && pathname !== "/");
             return (
               <Link
                 key={index}
-                className={`font-sans font-semibold text-lg text-center w-60 border-none rounded-full py-1 px-4 
-                    ${
-                      isActive
-                        ? "bg-blue-600 text-slate-100"
-                        : "bg-blue-500  text-slate-50 "
-                    }`}
+                className={`text-lg ${
+                  isactive ? "text-blue-600" : "text-slate-600"
+                }`}
                 href={link.href}
               >
                 {link.name}
               </Link>
             );
           })}
-        </nav>
+        </ul>
         {children}
       </div>
     </>
